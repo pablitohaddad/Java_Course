44813232cd6090a77f7d6b6c876b930b6b8d91ae
pablitohaddad <pablohaddad73@gmail.com>
2023-12-14 15:02:14 -0400

    Funcoes interessantes java

---
+++ EmAulaUdemy/OutrosTopicosJava/src/OperacaoSplit.java
@@ -0,0 +1,22 @@
+public class OperacaoSplit {
+    public static void main(String[] args) {
+
+        /*
+
+        COMENTARIO GRANDE
+
+        */
+
+        // COMENTARIO BREVE
+
+        String s = "potato apple lemon";
+        String[] vect = s.split(" ");
+        String word1 = vect[0];
+        String word2 = vect[1];
+        String word3 = vect[2];
+
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+    }
+}
