0681497665723114d538bc53dbbd0a7affcc21bb
pablitohaddad <pablohaddad73@gmail.com>
2023-12-20 15:54:54 -0400

    Solucao ruim do codigo de Excecoes

--- exercicios/ProblemaExemploExcecaoRuimBoa/src/model/entities/Reservation.java
+++ exercicios/ProblemaExemploExcecaoRuimBoa/src/model/entities/Reservation.java
@@ -43,9 +43,18 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut){
+    public String updateDates(Date checkIn, Date checkOut){ // Jogando os possiveis erros para a classe Reservation date
+
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)){
+            return "Error in reservation: Reservation dates for update must be future dates";
+        }
+        if(!checkOut.after(checkIn)){
+            return  "Error in reservation: Check-out date must be after check-in date";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null; // Se nao houver erro, nao retorna nada
     }
 
     @Override
